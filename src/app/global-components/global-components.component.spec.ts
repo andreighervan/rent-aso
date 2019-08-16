b8824540d4082e56927e6e6bbee56e182853cce8
andreighervan <andrei.ghervan8@gmail.com>
2019-08-16 17:14:37 +0300

    add global components

---
+++ src/app/global-components/global-components.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GlobalComponentsComponent } from './global-components.component';
+
+describe('GlobalComponentsComponent', () => {
+  let component: GlobalComponentsComponent;
+  let fixture: ComponentFixture<GlobalComponentsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GlobalComponentsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GlobalComponentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
